a351e5847306d75d7bb44805206e7cf14cd54fcc
Debra Winter <deb@debwinter.com>
2024-01-24 10:39:11 -0500

    commit from step 3.5

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Module2Review;
+﻿using System.Diagnostics;
+
+namespace Module2Review;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // TODO
+        // Create a C# console application that manages student grades. It should allow the following:
+            //Creates students
+            //Add grades
+            //Calculates a student’s average grade
+            //Displaying student information
+
+
     }
 }
 
---
+++ Module2Review/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Module2Review
+{
+//Create a class called Student
+
+    public class Student
+	{
+		public Student()
+		{
+		}
+
+        //Create a property called Name of type string
+        public string Name { get; set; }
+
+        //Create a property called ID of type int
+        public int ID { get; set; }
+
+        //Create a property called Grades of type List
+        public List<double> Grades = new List<double>();
+
+        //Create a method called AddGrade in the Student class
+        public void AddGrade(double grade)
+		{
+			Grades.Add(grade);
+		}
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+
+
+
+	}
+}
+
